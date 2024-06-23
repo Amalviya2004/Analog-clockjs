3b4a8a8d4ab31cd9989a8a3e9011c5754e60670d
Anurag malviya <143078854+Amalviya2004@users.noreply.github.com>
2024-06-23 10:55:34 +0530

    app.js

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-// const deg = 6;
-// const hr = document.querySelector("#hr");
-// const mn = document.querySelector("#mn");
-// const sc = document.querySelector("#sc");
-// setInterval(() => {
-//   let date = new Date();
-//   let ms = date.getMilliseconds() * deg;
-//   let hour = date.getHours() * 30;
-//   let min = date.getMinutes() * deg;
-//   let sec = date.getSeconds() * deg + ms / 1000;
-//   hr.style.transform = `rotateZ(${(hour) +( min / 12)}deg)`;
-//   mn.style.transform = `rotateZ(${min})deg`;
-//   sc.style.transform = `rotateZ(${sec})deg`;
-// }, 1);
 const deg = 6;
 
 const hr = document.querySelector("#hr");
